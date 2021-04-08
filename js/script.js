ece5a2606782fe5089d32f24da25b1f7c646ae2c
Michele Lisignoli <michele@lisignoli.com>
2021-04-08 15:52:53 +0200

    get the albums from the server and print it on the gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var app = new Vue({
-   el: "#app",
-   data:{
-   },
+var app = new Vue(
+   {
+      el: "#app",
+      data:{
+         albums: []
+      },
+      mounted: function() {
+         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+         .then((ServerAnswer) => {
+            this.albums = ServerAnswer.data.response;
+         });
+      }
 
-});
+   });
