a6e7d2959220654ae538a3c99202e3e289e9ed2c
Michele Lisignoli <michele@lisignoli.com>
2021-04-08 18:55:33 +0200

    select genre

--- js/script.js
+++ js/script.js
@@ -2,13 +2,28 @@ var app = new Vue(
    {
       el: "#app",
       data:{
-         albums: []
+         albums: [],
+         genres:  ["",],
+         select: "",
       },
       mounted: function() {
          axios.get("https://flynn.boolean.careers/exercises/api/array/music")
          .then((ServerAnswer) => {
             this.albums = ServerAnswer.data.response;
+            this.albums.forEach((album) => { 
+               if (this.genres.includes(album.genre) == false ){
+                  this.genres.push(album.genre)
+               }
+             });
          });
-      }
-
+      },
+      computed:{
+         filtredAlbums: function(){
+             return this.albums.filter((music) =>{
+                 return music.genre.match(this.select);
+             });
+         }
+     }
+   
    });
+   // Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
